8abd56979977bb2836e853bacff722d1e432cc52
Nethmi97 <nnimesha97@gmail.com>
2023-12-08 19:59:09 +0530

    Initial commit

---
+++ src/main/java/com/commitmanagementservice/commitmanagementservice/CommitManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.commitmanagementservice.commitmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommitManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommitManagementServiceApplication.class, args);
+	}
+
+}
